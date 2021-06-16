ce7fb107f65cf6a350a744c81d8492c8f4d49e2b
hasser0 <osvaldoes.unam@gmail.com>
2021-06-16 10:24:55 -0500

    Exhaustivo

--- exhaustive.py
+++ exhaustive.py
@@ -10,18 +10,18 @@ def exhaustive(Pijk, Cik):
     for politic in product(decisions, repeat=m):
         P = np.array([Pijk[i, :, k] for i, k in enumerate(politic)])
         C = np.array([Cik[i, k] for i, k in enumerate(politic)])
-
         if np.isnan(P).any() or np.isnan(C).any():
             continue
         PI = stable_vector(P)
         cur_z = (C @ PI)[0]
         print("-" * 15 + "Política " + str(politic) + "-" * 15)
-        print(P)
-        print(C)
         print(PI)
         print(cur_z)
         print()
         if cur_z < z_best:
             z_best = cur_z
             politic_best = politic
+    print("-"*15+"Mejor política"+"-"*15)
+    print(politic_best)
+    print(z_best)
     return np.array(politic_best), z_best
--- main.py
+++ main.py
@@ -20,13 +20,11 @@ if __name__ == '__main__':
     m = args.states
     k = args.decisions
     method = args.method
-    pij = tuple(df.values[m*i:m*(i+1),0:] for i in range(k))
+    pij = tuple(df.values[m*i:m*(i+1),0:m] for i in range(k))
     Pijk = np.dstack(pij)
     Cik = df.values[m*k:, 0:k]
     if method == 'exhaustive':
-        best, z = exhaustive(Pijk, Cik)
-        print(best)
-        print(z)
+        exhaustive(Pijk, Cik)
     if method == 'linear':
         best, z = lp(Pijk, Cik)
         print(best.reshape(m,k))
